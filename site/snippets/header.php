ffa8203fe64152c65f0d9803e3e7c14a5f5c9b08
SoaPatrick <patrick.leisinger@gmail.com>
2022-04-18 14:57:45 +0200

    [FIX] fix title in head

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?= $page->isHomePage() ? $site->title() : $page->title() . ' |  ' .$site->title(); ?></title>
+  <title><?= $page->isHomePage() ? $site->title() : $page->title() . ' | ' .$site->title(); ?></title>
   <?= vite()->client() ?>
   <?= vite()->css() ?>
 </head>
