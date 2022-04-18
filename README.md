e0d1efb671771be05be31a2effac3f9bd1430ead
SoaPatrick <patrick.leisinger@gmail.com>
2022-04-18 14:40:05 +0200

    [ADD] add initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Kirby Vite Starter
+
+## Installation
+
+Clone this repository and run:
+
+```
+composer install
+```
+
+```
+npm install
+```
+
+## Development
+
+Start vite's dev server and a simple php dev server by running:
+
+```
+npm run dev
+```
+
+Visit `localhost:8888` in the browser. Vite's dev server (`localhost:3000`) is only used for serving js, css and assets.
+
+## Production
+
+Build your optimized frontend assets to `public/dist`:
+
+```
+npm run build
+```
---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import liveReload from 'vite-plugin-live-reload';
+import { resolve } from 'path';
+
+export default ({ mode }) => ({
+  root: 'src',
+  base: mode === 'development' ? '/' : '/dist/',
+
+  server: {
+    port: 3000,
+    strictPort: true,
+  },
+
+  build: {
+    outDir: resolve(process.cwd(), 'public/dist'),
+    emptyOutDir: true,
+    manifest: true,
+    rollupOptions: {
+      input: resolve(process.cwd(), 'src/index.js'),
+    },
+  },
+
+  plugins: [
+    liveReload(['content/**/*', 'site/**/*.php'], { root: process.cwd() }),
+  ],
+});
