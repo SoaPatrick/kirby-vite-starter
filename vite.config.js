2d35567acfd4489defdb08b2645716a7297f1d80
SoaPatrick <patrick.leisinger@gmail.com>
2022-11-16 12:37:22 +0100

    [UPDATE] update to kirby 3.8 and vite 3.0

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,15 @@
-import liveReload from 'vite-plugin-live-reload';
 import { resolve } from 'path';
+import kirby from 'vite-plugin-kirby';
 
 export default ({ mode }) => ({
   root: 'src',
   base: mode === 'development' ? '/' : '/dist/',
 
-  server: {
-    port: 3000,
-    strictPort: true,
-  },
-
   build: {
     outDir: resolve(process.cwd(), 'public/dist'),
     emptyOutDir: true,
-    manifest: true,
-    rollupOptions: {
-      input: resolve(process.cwd(), 'src/index.js'),
-    },
+    rollupOptions: { input: resolve(process.cwd(), 'src/index.js') },
   },
 
-  plugins: [
-    liveReload(['content/**/*', 'site/**/*.php'], { root: process.cwd() }),
-  ],
+  plugins: [kirby()],
 });
